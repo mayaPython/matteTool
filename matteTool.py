e999b2e135fc83ca6d0c02e1c0f8ff264542e45d
mayaPython <mayaPython@users.noreply.github.com>
2024-05-14 08:10:35 +0100

    Add files via upload

--- matteTool.py
+++ matteTool.py
@@ -1,54 +1,57 @@
 '''
 
 written by Emily Pollacchi
-  	file name alignX
-  	Copyright (C) 2016 by Emily Pollacchi
+  	file name matteTool
+  	Copyright (C) 2024 by Emily Pollacchi
   	epollacchi@gmail.com
 
 '''
 
-import maya.cmds as mc
+import maya.cmds as cmds
 
 def matteCreator():
     
-    matteName   = mc.textField('matteName', query = True, text = True)
+    matteName   = cmds.textField('matteName', query = True, text = True)
     if len(matteName) == 0:
         matteName = 'Matte_01'
     if matteName.isdigit():
         matteName = 'Matte_01'
         
-    widthInput  = mc.floatField('widthInput', query = True, value = True)
-    heightInput = mc.floatField('heightInput', query = True, value = True)
+    widthInput  = cmds.floatField('widthInput', query = True, value = True)
+    heightInput = cmds.floatField('heightInput', query = True, value = True)
 
-    myPlane = mc.polyPlane(n = matteName, sx=1, sy=1, w = widthInput, h = heightInput)
+    myPlane = cmds.polyPlane(n = matteName, sx=1, sy=1, w = widthInput, h = heightInput)
     
-    mc.select(myPlane)
-    mc.rotate('90deg', 0, 0, r=True )
+    cmds.select(myPlane)
+    cmds.rotate('90deg', 0, 0, r=True)
+
     
-    curSel = mc.ls(myPlane)
+    curSel = cmds.ls(myPlane)
     for n in curSel:
-        bbox = mc.exactWorldBoundingBox(n)
-        bottom = [(bbox[0] + bbox[3])/2, bbox[1], (bbox[2] + bbox[5])/2]
-        mc.xform(n, piv=bottom, ws=True)
-                
+        bbox = cmds.exactWorldBoundingBox(n)
+        lower = [(bbox[0] + bbox[3])/2, bbox[1], (bbox[2] + bbox[5])/2]
+        cmds.xform(n, piv=lower, ws=True)
+        cmds.move(0,0,0, ws=True, rpr=True)
+        cmds.makeIdentity(apply=True, translate=True, rotate=True, scale=True)
+	                
 def myWindow():
        
-    if mc.window('matteWindow', exists = True):
-        mc.deleteUI('matteWindow')
+    if cmds.window('matteWindow', exists = True):
+        cmds.deleteUI('matteWindow')
       
-    mc.window('matteWindow', title = 'Matte Creator', widthHeight = (122, 130), mnb = False, s = False, mxb = False, rtf = False)
-    mc.columnLayout()
+    cmds.window('matteWindow', title = 'Matte Creator', widthHeight = (122, 130), mnb = False, s = False, mxb = False, rtf = False)
+    cmds.columnLayout()
     
-    mc.text(label = 'Enter Matte Name:')
-    mc.textField('matteName', pht = 'Matte_01')
+    cmds.text(label = 'Enter Matte Name:')
+    cmds.textField('matteName', pht = 'Matte_01')
     
-    mc.text(label = 'Enter Width:')
-    mc.floatField('widthInput', minValue = 0.001, value = 19.2, precision = 2, step = 0.01)
+    cmds.text(label = 'Enter Width:')
+    cmds.floatField('widthInput', minValue = 0.001, value = 19.2, precision = 2, step = 0.01)
     
-    mc.text(label = 'Enter Height:')
-    mc.floatField('heightInput', minValue = 0.001, value = 10.8, precision = 2, step = 0.01)
+    cmds.text(label = 'Enter Height:')
+    cmds.floatField('heightInput', minValue = 0.001, value = 10.8, precision = 2, step = 0.01)
     
-    mc.button(label = 'Create', command = 'matteCreator()')
-    mc.showWindow()
-    
+    cmds.button(label = 'Create', command = 'matteCreator()')
+    cmds.showWindow()
+        
 myWindow()
